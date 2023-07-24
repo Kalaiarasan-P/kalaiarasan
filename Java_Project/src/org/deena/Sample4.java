98155d65c31e8d031601c035411d72356723206b
deenat423 <deenat423@gmail.com>
2023-07-24 16:54:16 +0530

    starting project

---
+++ Java_Project/src/org/deena/Sample4.java
@@ -0,0 +1,89 @@
+package org.deena;
+
+public class Sample4 {
+
+	public static void main(String[] args) {
+	/*	int empID=42;
+		if (empID==42) {
+			System.out.println("valid");
+		}else {
+			System.out.println("not valid");
+		}
+*/	
+	/*	for (int i = 1; i <=5; i++) {
+			for (int j = 5; j >=1; j--) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}*/
+		/*
+		for (int i = 1; i <=10; i++) {
+			if (i==5) {
+				break;
+			}
+			System.out.println(i);
+		}
+		System.out.println("contunie");
+		
+		for (int i = 1; i <=10; i++) {
+			if (i==5) {
+				continue;	
+			}
+			System.out.println(i);
+		}
+		*/
+		
+		/*for (int i = 0; i <=20; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+		}
+		*/
+		
+		/*int count=0;
+		for (int i = 1; i <=100; i++) {
+			if (i%2==0) {
+			count=count+1;
+			}
+		}
+		System.out.println(count);
+		*/
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 5; j >= i; j--) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+
+}
---
+++ Java_Project/src/org/deena/Sample6.java
@@ -0,0 +1,12 @@
+package org.deena;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample6 extends Sample5 {
+	public static void main(String[] args) {
+		List<String> emp=new ArrayList<String>();
+		
+	}
+
+}
